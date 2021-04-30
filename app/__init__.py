27724d6f3f7829deb2be79be7a175841462014f2
alvynah <alvynahw@gmail.com>
2021-04-30 14:20:55 +0300

    Adding a secret key and quotes base api url

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from config import DevConfig
 
 #Initializing the application
-app=Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
+#setting up configurations
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 from app import views

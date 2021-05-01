deb98c07bb6f99cefa24f19166b52749599600cc
alvynah <alvynahw@gmail.com>
2021-05-01 09:45:31 +0300

    Add profile page to display writer's profile and their posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from . import main
 from ..request import get_quotes
-from ..models import Quote
+from ..models import Quote,User
+from .forms import UpdateProfile
+from .. import db
 from flask_login import login_required
 
 @main.route('/')
@@ -14,3 +16,33 @@ def index():
 
 
     return render_template('index.html',quotes=quotes)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
+
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)

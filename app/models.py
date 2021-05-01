6e608f49a42e5c632047033ca950c3f69e4bad0d
alvynah <alvynahw@gmail.com>
2021-05-01 06:46:37 +0300

    Create the user model, connect the database and use flask-script shell

--- app/models.py
+++ app/models.py
@@ -1,5 +1,15 @@
+from . import db
+
 class Quote:
     
     def __init__(self,author,quote):
         self.author=author
         self.quote=quote
+class User (db.Model):
+    __tablename__ ='users'
+    id= db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
 
     QOUTES_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://cocoh:kitkAt10@localhost/blogomatic'
+
 
 
 class ProdConfig(Config):

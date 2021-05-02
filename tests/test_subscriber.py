585c00364bbddbcc3c2d3c5ba3de10d3d7dea700
alvynah <alvynahw@gmail.com>
2021-05-02 10:11:24 +0300

    Test for class Subscriber

---
+++ tests/test_subscriber.py
@@ -0,0 +1,24 @@
+import unittest
+from app.models import Subscriber
+from app import db
+
+
+class SubscriberModelTest(unittest.TestCase):
+    """
+    Test Class to test the behaviour of the Comment class
+    """
+
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.subscriber = Subscriber(email='alvynahw@gmail.com')
+
+    def tearDown(self):
+        Subscriber.query.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.subscriber, Subscriber))
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.subscriber.email, 'alvynahw@gmail.com')
